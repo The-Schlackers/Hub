bbd74576736bb75833ee82bee390f3c11b3113ae
Estemyr <isac870@hotmail.com>
2018-12-03 15:55:06 +0100

    Part 2
    2

---
+++ The Schlackers hub/Assets/Scripts/LoadingScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScript : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        StartCoroutine(LoadAsyncScene());
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    IEnumerator LoadAsyncScene()
+    {
+
+        if (PlayerPrefs.HasKey("SceneToLoad") == false)
+            yield break; // ha detta så tidigt som möjligt // Sealisera stora saker ist för playerprefs
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync(PlayerPrefs.GetInt("SceneToLoad"));
+        
+        while (!operation.isDone)
+        {
+            yield return null;
+        }
+    }
+}
---
+++ The Schlackers hub/Assets/Scripts/Scri.cs
@@ -0,0 +1,20 @@
+﻿using Assets.Scripts.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scri : MonoBehaviour {
+ 
+
+	// Use this for initialization
+	void Start () {
+        GameUtils.LoadScene(0);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+       
+    }
+}
